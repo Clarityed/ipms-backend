2dadf9f6fde6a848ac4217addf9f0e06a658b2a4
clarity <2270893459@qq.com>
2023-02-19 10:20:03 +0800

    Initial commit

---
+++ src/test/java/com/clarity/ipmsbackend/IpmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clarity.ipmsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpmsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
